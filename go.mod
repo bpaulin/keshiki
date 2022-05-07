62f51e974059be0f43b87a3e5e60d09e897040e0
bpaulin <brunopaulin@bpaulin.net>
2022-05-07 13:58:54 +0200

    yaml load

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module bpaulin/keshiki
 
 go 1.18
 
-require github.com/go-resty/resty/v2 v2.7.0
+require (
+	github.com/go-resty/resty/v2 v2.7.0
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
 
 require golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
--- hello.go
+++ hello.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 
 	"github.com/go-resty/resty/v2"
+
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
+	// Yaml load
+	yfile, err := ioutil.ReadFile("config.yaml")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := make(map[interface{}]interface{})
+
+	err2 := yaml.Unmarshal(yfile, &data)
+
+	if err2 != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("--- t:\n%v\n\n", data)
+
+	// rest client
 	client := resty.New()
 	resp, err := client.R().
 		EnableTrace().
