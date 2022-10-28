bd978432d5fd0f2d583740c66363c7e549b47ac5
Bruno Paulin <brunopaulin@bpaulin.net>
2022-10-28 18:56:14 +0200

    configs

--- main.go
+++ main.go
@@ -3,32 +3,41 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/adrg/xdg"
-	"github.com/bpaulin/keshiki/cmd"
 	"github.com/spf13/viper"
+
+	"github.com/bpaulin/keshiki/cmd"
 )
 
 func loadConfig() {
+	// config format
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
+
+	// config path
+	configFilePath, err := xdg.ConfigFile("keshiki")
+	if err == nil {
+		log.Print(configFilePath)
+		viper.AddConfigPath(configFilePath)
+		os.Mkdir(configFilePath, os.ModePerm)
+	}
 	viper.AddConfigPath(".")
 
-	configFilePath, err := xdg.ConfigFile("keshiki")
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		viper.AddConfigPath(configFilePath)
-	}
-
+	// read or init config
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
 			log.Print("no config file found")
+			if err := viper.SafeWriteConfig(); err != nil {
+				log.Print(err)
+			}
 		} else {
 			panic(fmt.Errorf("fatal error config file: %w", err))
 		}
 	}
 }
+
 func main() {
 	loadConfig()
 	cmd.Execute()
